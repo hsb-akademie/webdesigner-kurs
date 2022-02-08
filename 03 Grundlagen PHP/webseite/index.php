a8ebd4e35a543dc2f44190dce404ef26d6a82d80
Dstemberger <30551405+SeoulHyun@users.noreply.github.com>
2022-02-08 15:21:58 +0100

    Erste Beispiele  aus dem Kurs

---
+++ 03 Grundlagen PHP/webseite/index.php
@@ -0,0 +1,24 @@
+<?php
+
+// Ausgabe
+print_r("HSB");
+echo "HSB";
+
+// Variable deklarieren
+$variable = 1;
+$variable2 = 'HSB';
+
+// Variablen verwenden
+$zahl1 = 10;
+$zahl2 = 20;
+
+$zahl3 = $zahl1 + $zahl2;
+echo $zahl3; // Ausgabe: 30
+
+// Funktionen mit return und Parametern
+function sum($var1, $var2)
+{
+    return $var1 + $var2;
+}
+
+echo sum(10, 20); // Ausgabe: 30
